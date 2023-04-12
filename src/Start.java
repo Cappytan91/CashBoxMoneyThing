ec1980bea1241a008815e7f79841557ffb460f5e
js4678 <js4678@k12.sd.us>
2023-04-12 14:15:27 -0500

    idk

--- src/Start.java
+++ src/Start.java
@@ -1,16 +1,38 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Start {
+
+    public Menu menu;
+    public Menu order;
+
     public static void main(String[] args) {
          new Start();
     }
 
     public Start(){
-        Menu menu = new Menu(new ArrayList<>());
-        Menu order = new Menu();
-        menu.addMenuItem( new Item("Coke", 0.99, "Just a regular Coke Bruh"));
+        this.menu = new Menu(new ArrayList<>());
+        this.order = new Menu();
 
-        System.out.println(menu.getMenuItem("Coke").getName() + "\n\n" + menu.getMenuItem("Hipster's Delight").getPrice());
+        loop();
+    }
+
+    public void loop(){
+        Scanner scan = new Scanner(System.in);
+        String input = "";
+        System.out.println("What would you like to add to your order?");
+        while (!input.toLowerCase().equals("done")){
+            input = scan.nextLine();
+
+            try{
+                order.addMenuItem(menu.getMenuItem(input));
+            }catch (Exception e){
+                System.out.println("Sorry we dont have that, is there anything else i can help with?");
+            }
+
+        }
+
+
     }
 
 }
