e46595e7f69bf4934e0cf0ae452a15962d00292f
js4678 <js4678@k12.sd.us>
2023-04-19 09:59:55 -0500

    UI + order struff

--- src/auhhhh.java
+++ src/auhhhh.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 /**
  *
  * @author js4678
@@ -15,6 +17,7 @@ public class auhhhh extends javax.swing.JFrame {
     public auhhhh() {
         initComponents();
         this.start = new Start();
+        this.Order = "Order:";
     }
 
     /**
@@ -44,6 +47,8 @@ public class auhhhh extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
 
@@ -156,6 +161,12 @@ public class auhhhh extends javax.swing.JFrame {
         jLabel2.setText("Total:");
         jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
 
+        jLabel3.setText("Subtotal:");
+        jLabel3.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+
+        jLabel4.setText("Tax: ");
+        jLabel4.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -185,7 +196,9 @@ public class auhhhh extends javax.swing.JFrame {
                                 .addGap(28, 28, 28)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                         .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE))
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE)
+                                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addContainerGap(213, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -212,8 +225,12 @@ public class auhhhh extends javax.swing.JFrame {
                                                         .addComponent(jButton12, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addContainerGap())
         );
 
         MenuTab.addTab("Menu", jPanel1);
@@ -226,7 +243,7 @@ public class auhhhh extends javax.swing.JFrame {
         );
         jPanel2Layout.setVerticalGroup(
                 jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 284, Short.MAX_VALUE)
+                        .addGap(0, 328, Short.MAX_VALUE)
         );
 
         MenuTab.addTab("tab2", jPanel2);
@@ -239,7 +256,7 @@ public class auhhhh extends javax.swing.JFrame {
         );
         jPanel3Layout.setVerticalGroup(
                 jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 284, Short.MAX_VALUE)
+                        .addGap(0, 328, Short.MAX_VALUE)
         );
 
         MenuTab.addTab("tab3", jPanel3);
@@ -252,7 +269,7 @@ public class auhhhh extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(MenuTab, javax.swing.GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE)
+                        .addComponent(MenuTab)
         );
 
         pack();
@@ -301,8 +318,21 @@ public class auhhhh extends javax.swing.JFrame {
     }
 
     public void writeToOrder(){
-        Order += "<br/>" + start.order.getMenuItem(start.order.getItems().size()-1).getName() + "<br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;$" + String.format("%.2f",start.order.getMenuItem(start.order.getItems().size()-1).getPrice());
+        Order += "<br/>&emsp;&emsp;" + start.order.getMenuItem(start.order.getItems().size()-1).getName() + "<br/>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;$" + String.format("%.2f",start.order.getMenuItem(start.order.getItems().size()-1).getPrice());
         jLabel1.setText("<html>" + Order + "</html>");
+
+
+        double tax = 0;
+        double sub = 0;
+
+        for (Item i: start.order.getItems()) {
+            sub += i.getPrice();
+        }
+        tax = sub * 0.065;
+
+        jLabel3.setText("Subtotal: $" + String.format("%.2f", sub));
+        jLabel4.setText("Tax: $" + String.format("%.2f", tax));
+        jLabel2.setText("Total: $" + String.format("%.2f", sub + tax));
     }
 
     /**
@@ -314,22 +344,24 @@ public class auhhhh extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+
+        FlatLightLaf.setup();
+    //    try {
+    //        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+    //            if ("Nimbus".equals(info.getName())) {
+    //                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+    //                break;
+    //            }
+    //        }
+    //    } catch (ClassNotFoundException ex) {
+    //        java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    //    } catch (InstantiationException ex) {
+    //        java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    //    } catch (IllegalAccessException ex) {
+    //        java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    //    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+    //        java.util.logging.Logger.getLogger(auhhhh.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    //    }
         //</editor-fold>
 
         /* Create and display the form */
@@ -363,6 +395,8 @@ public class auhhhh extends javax.swing.JFrame {
     public Start start;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
 
     // End of variables declaration
 }
